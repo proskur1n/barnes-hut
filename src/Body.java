383a6e78c27d468b534f1a3d4608fa3d4c89157a
Andrey Proskurin <andreyproskurin@protonmail.com>
2022-05-01 20:56:21 +0200

    Add Body and Vector3 classes

---
+++ src/Body.java
@@ -0,0 +1,94 @@
+import codedraw.CodeDraw;
+import codedraw.Palette;
+import java.awt.Color;
+
+public class Body {
+
+    private Vector3 position;
+    private Vector3 velocity;
+    private Vector3 force;
+    private double mass;
+    private Color color;
+
+    private static final Color[] palette = {
+            Palette.BISQUE,
+            Palette.TURQUOISE,
+            Palette.STEEL_BLUE
+    };
+
+    public Body(double mass, Vector3 position, Vector3 velocity) {
+        this.position = position;
+        this.velocity = velocity;
+        this.force = new Vector3();
+        this.mass = mass;
+        this.color = getRandomColor();
+    }
+
+    // Returns the Morton code based on this bodie's position.
+    public long getMortonCode(Vector3 origin, double size) {
+        double x = (position.x - origin.x) / size;
+        double y = (position.y - origin.y) / size;
+        double z = (position.z - origin.z) / size;
+        return MortonCode.get(x, y, z);
+    }
+
+    public double distanceSqrd(Body other) {
+        return position.distanceSqrd(other.position);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    // Adds the gravitational force exerted by 'b' on this body. The current
+    // position and velocity remain unchanged until you call update.
+    public void addGravitationalForceFrom(Body b) {
+        // TODO make faster (this one works)
+        Vector3 direction = b.position.minus(position);
+        double distance = direction.length();
+        direction.normalize();
+        double force = Simulation.G * mass * b.mass / (distance * distance);
+        this.force = this.force.plus(direction.times(force));
+    }
+
+    // Moves the body according to its current force and velocity. Afterwards,
+    // this bodie's force is reset to 0.
+    public void update(double deltaTime) {
+        Vector3 acceleration = force.times(1 / mass); // F = m*a -> a = F/m
+        Vector3.multThenAdd(velocity, acceleration, deltaTime);
+        Vector3.multThenAdd(position, velocity, deltaTime);
+        force.setToZero();
+    }
+
+    // Returns an incomplete copy of this body that can be used to calculate
+    // gravitational force. There is only a small subset of methods that you can
+    // call on a partial body.
+    public Body makePartialCopy() {
+        return new Body(mass, position);
+    }
+
+    // Merges the mass and position of bodies 'a' and 'b' and saves the result
+    // in 'a'. Body 'b' remains unchanged.
+    public static void partialMerge(Body a, Body b) {
+        double f = b.mass / (a.mass + b.mass);
+        Vector3.mix(a.position, b.position, f);
+        a.mass = a.mass + b.mass;
+    }
+
+    public void draw(CodeDraw cd) {
+        cd.setColor(color);
+        // TODO remove this mass to radius
+        position.drawAsFilledCircle(cd, SpaceDraw.massToRadius(mass));
+    }
+
+    // Used by makePartialCopy.
+    private Body(double mass, Vector3 position) {
+        this.mass = mass;
+        this.position = new Vector3(position);
+    }
+
+    private static Color getRandomColor() {
+        int i = (int) (Math.random() * palette.length);
+        return palette[i];
+    }
+}
---
+++ src/Vector3.java
@@ -0,0 +1,93 @@
+import codedraw.CodeDraw;
+
+public class Vector3 {
+
+    public double x;
+    public double y;
+    public double z;
+
+    public Vector3() {
+    }
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3(Vector3 v) {
+        this.x = v.x;
+        this.y = v.y;
+        this.z = v.z;
+    }
+
+    public Vector3(double value) {
+        this.x = value;
+        this.y = value;
+        this.z = value;
+    }
+
+    public void setToZero() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public Vector3 plus(Vector3 v) {
+        return new Vector3(x + v.x, y + v.y, z + v.z);
+    }
+
+    public Vector3 minus(Vector3 v) {
+        return new Vector3(x - v.x, y - v.y, z - v.z);
+    }
+
+    public Vector3 times(double d) {
+        return new Vector3(x * d, y * d, z * d);
+    }
+
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public void normalize() {
+        double len = length();
+        x /= len;
+        y /= len;
+        z /= len;
+    }
+
+    public double distanceSqrd(Vector3 v) {
+        return (v.x - x) * (v.x - x) + (v.y - y) * (v.y - y) + (v.z - z) * (v.z - z);
+    }
+
+    public String toString() {
+        return String.format("[%g, %g, %g]", x, y, z);
+    }
+
+    // Draws a filled circle with a specified radius centered at the (x,y)
+    // coordinates of this vector in the canvas associated with 'cd'. The
+    // z-coordinate is not used.
+    public void drawAsFilledCircle(CodeDraw cd, double radius) {
+        double factor = cd.getWidth() / Simulation.SECTION_SIZE;
+        cd.fillCircle(
+                (x + Simulation.SECTION_SIZE / 2) * factor,
+                (y + Simulation.SECTION_SIZE / 2) * factor,
+                Math.max(radius * factor, 1.5));
+    }
+
+    // Calculates a + b * m and stores the result in a.
+    public static void multThenAdd(Vector3 a, Vector3 b, double m) {
+        a.x += b.x * m;
+        a.y += b.y * m;
+        a.z += b.z * m;
+    }
+
+    // Performs linear interpolation of vectors 'a' and 'b' with the coefficient
+    // 'f', and stores the result in 'a'. Result is calculated as
+    // a = a * (1 - f) + b * f.
+    public static void mix(Vector3 a, Vector3 b, double f) {
+        a.x = a.x * (1.0 - f) + b.x * f;
+        a.y = a.y * (1.0 - f) + b.y * f;
+        a.z = a.z * (1.0 - f) + b.z * f;
+    }
+}
